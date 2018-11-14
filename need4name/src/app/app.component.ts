586648a3bedd5749c0474e8d95fccac4a6288f2b
Michał Łuniewski <m.luniewski@i4b.pl>
2018-11-14 22:50:39 +0100

    First experiments with AngularFire

--- need4name/src/app/app.component.ts
+++ need4name/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'need4name';
+  restaurants: Observable<any[]>;
+  orders: Observable<any[]>;
+
+  constructor(db: AngularFirestore) {
+    this.restaurants = db.collection('restaurants').valueChanges();
+    this.orders = db.collection('orders').valueChanges();
+  }
 }
--- need4name/src/app/app.module.ts
+++ need4name/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
